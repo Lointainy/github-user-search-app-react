4730496ca37836e320702daa674fbfe1f4aaef11
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-30 18:39:52 +0300

    added config for partials

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @use "sass:math";
+        @import "@/assets/scss/partials";
+        `,
+      },
+    },
+  },
 })
