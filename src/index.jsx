90a0066b4908a4668ef4d5f3beab1effca286703
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-30 20:40:32 +0300

    added font-awesome icons

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMoon, faSun, faSearch, faLocationDot, faLink } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab, faMoon, faSun, faSearch, faLocationDot, faLink)
 
 /* Style */
 import '@/assets/scss/main.scss'
