4340f407ecbbe01f5fa2e2f765c41f091728f46c
Eugene Kozakov <lointainyyy@gmail.com>
2022-07-31 14:43:43 +0300

    Revert "added icon"
    This reverts commit 40da259a3a19c2533eb85d06bfe2006058576acc.

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMoon, faSun, faSearch, faLocationDot, faLink, faBuilding } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun, faSearch, faLocationDot, faLink } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fab, faMoon, faSun, faSearch, faLocationDot, faLink, faBuilding)
+library.add(fab, faMoon, faSun, faSearch, faLocationDot, faLink)
 
 /* Style */
 import '@/assets/scss/main.scss'
